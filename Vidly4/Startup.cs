d4078c67a59ff69d888731264861542eccaa684b
Noriel <norielsg1214@gmail.com>
2021-08-20 18:37:08 -0400

    Initial Commit

---
+++ Vidly4/Startup.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Vidly4.Startup))]
+namespace Vidly4
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
+
+/*
+echo "# MVC5Application4" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git branch -M main
+git remote add origin https://github.com/noriel1214/MVC5Application4.git
+git push -u origin main
+*/
